e68b1fb2e84ace0e3223140ba8d9a7e2f4160450
GabrielLoureiro <carinbo@hotmail.com>
2021-07-14 17:43:56 -0300

    teste python reverso

---
+++ PythonReverse.py
@@ -0,0 +1,23 @@
+def reverse(string):
+    string = "".join(reversed(string))
+    return string
+  
+s = "teste inicial"
+  
+print ("The original string  is : ",end="")
+print (s)
+  
+print ("The reversed string(using reversed) is : ",end="")
+print (reverse(s))
+
+def reverse(string):
+    string = "".join(reversed(string))
+    return string
+  
+s = "teste inicial 2 "
+  
+print ("The original string  is : ",end="")
+print (s)
+  
+print ("The reversed string(using reversed) is : ",end="")
+print (reverse(s))
